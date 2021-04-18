39129d362136249ddcd5e40e5d4e4d69c3e5a7f4
mohmmadnoorjebreen <mohammadnoormjk1998@gmail.com>
2021-04-18 20:42:07 +0300

    problem3 solved

--- class-05/lab-a/starter-code/app.js
+++ class-05/lab-a/starter-code/app.js
@@ -30,11 +30,14 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiply(a, b) { //eslint-disable-line
+  let multiply = a * b;
+  let msg = 'The product of '+ a +' and '+ b +' is '+ multiply +'.';
+  return [multiply, msg]
 
 }
 
 // Here is the test for multiply(); uncomment it to run it
-// testMultiply(5,9);
+testMultiply(5,9);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -51,11 +54,20 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sumAndMultiply(a, b, c) { //eslint-disable-line
-
+  let x = sum(a,b);
+  let sumAndMultiplyS = sum(x[0],c);
+  let y = multiply(a,b)
+  let sumAndMultiplyM = multiply(y[0],c);
+  let msg =  a +' and '+ b +' and '+ c +' sum to '+ sumAndMultiplyS[0] +'.';
+  let msg2 = 'The product of '+ a +' and '+ b +' and '+c+' is '+ sumAndMultiplyM[0] +'.';
+  
+  return [sumAndMultiplyS[0],sumAndMultiplyM[0],msg,msg2]
 }
 
+
+
 // Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
